fd62a1ea53b2c78385cba5bc60a7e0be0367b590
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2025-07-08 18:41:11 +0530

    hoja

--- RafiFigma-figma/src/components/common/Footer.jsx
+++ RafiFigma-figma/src/components/common/Footer.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Typical from 'react-typical';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Footer.css'; // optional if you want to move custom CSS there

ccbaa853627ed3bdf63d065b202155873cafe1ef
nexromedia <nexromedia@gmail.com>
2025-07-20 21:01:28 +0530

    project added

--- RafiFigma-figma/src/Routes.jsx
+++ RafiFigma-figma/src/Routes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Service from './pages/ServicePage/Service';
 import Work from './pages/WorkPage/Work';
-
+import ProjectsPage from "./pages/OurProjects/ProjectsPage";
 const AppRoutes = () => {
   return (
     <Router>
@@ -11,7 +11,8 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
         <Route path="/work" element={<Work />} />
-        
+        <Route path="/projects" element={<ProjectsPage />} />
+
         {/* Add more routes here as needed */}
       </Routes>
     </Router>

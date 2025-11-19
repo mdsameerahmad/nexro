8b11c0d4fcd469b024c1faa75caea47a7af1bc7f
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2025-11-19 16:07:00 +0530

    ready to push

--- RafiFigma-figma/src/Routes.jsx
+++ RafiFigma-figma/src/Routes.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Service from './pages/ServicePage/Service';
 import Work from './pages/WorkPage/Work';
 
+import ScrollToTop from './ScrollToTop';
+
 const AppRoutes = () => {
   return (
     <Router>
+      <ScrollToTop />  {/* FIX: ScrollToTop inside Router */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service />} />
         <Route path="/work" element={<Work />} />
-        
-        {/* Add more routes here as needed */}
       </Routes>
     </Router>
   );
